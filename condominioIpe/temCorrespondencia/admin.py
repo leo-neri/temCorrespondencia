99df8d16168791d7466ace2fe80cab9e1d91a786
leomestre <leoneriribeiro@gmail.com>
2021-03-05 03:12:23 -0300

    Commit 2 - App v1 pronto!

--- condominioIpe/temCorrespondencia/admin.py
+++ condominioIpe/temCorrespondencia/admin.py
@@ -4,9 +4,9 @@ from .models import Encomenda
 # Register your models here.
 @admin.register(Encomenda)
 class EncomendaAdmin(admin.ModelAdmin):
-    list_display = ('morador', 'tipo', 'recebimento', 'retirada', 'status')
+    list_display = ('morador', 'tipo', 'recebimento', 'status', 'autor')
     list_filter = ('morador', 'tipo', 'status')
     search_fields = ('morador', 'tipo')
-    # raw_id_fields = ('morador',)
+    raw_id_fields = ('autor',)
     date_hierarchy = 'recebimento'
     ordering = ('status', 'recebimento')
